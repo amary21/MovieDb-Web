5221645ecaff4d49f5cb57d99ea2b2c41d571807
amary21 <taufik.amary@gmail.com>
2020-05-22 19:24:44 +0700

    popularity

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,117 +1,107 @@
 function main() {
 
-    const baseUrl = "https://web-server-book-dicoding.appspot.com";
+    const baseUrl = "https://api.themoviedb.org/3/movie";
+    const baseUrlImage = "https://image.tmdb.org/t/p/original";
+    const API_KEY = "ce3747f9814de0e3fc3292c9ef36fcdb";
 
-    const getBook = async () => {
+    const getPopular = async () => {
         try{
-            const response = await fetch(`${baseUrl}/list`);
+            const url = ''.concat(baseUrl, `/popular`, `?api_key=${API_KEY}`);
+            const response = await fetch(`${url}`);
             const responseJson = await response.json();
             if(responseJson.error){
                 showResponseMessage(responseJson.message);
             }else{
-                renderAllBooks(responseJson.books);
+                renderPopular(responseJson.results);
             }
         } catch(error){
             showResponseMessage(error);
         }
     };
 
-
-    const insertBook = (book) => {
-        const xhr = new XMLHttpRequest();
-
-        xhr.onload = function(){
-            const responseJson = JSON.parse(this.responseText);
-            showResponseMessage(responseJson.message);
-            getBook();
+    const getNowplaying = async () => {
+        try{
+            const url = ''.concat(baseUrl, `/now_playing`, `?api_key=${API_KEY}`);
+            const response = await fetch(`${url}`);
+            const responseJson = await response.json();
+            if(responseJson.error){
+                showResponseMessage(responseJson.message);
+            }else{
+                console.log(responseJson.results);
+            }
+        } catch(error){
+            showResponseMessage(error);
         }
-
-        xhr.open("POST", `${baseUrl}/add`);
-
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.setRequestHeader("X-Auth-Token", "12345");
-
-        xhr.send(JSON.stringify(book));
     };
 
-    const updateBook = (book) => {
-        const xhr = new XMLHttpRequest();
-
-        xhr.onload = function(){
-            const responseJson = JSON.parse(this.responseText);
-            showResponseMessage(responseJson.message);
-            getBook();
+    const getUpcoming = async () => {
+        try{
+            const url = ''.concat(baseUrl, `/upcoming`, `?api_key=${API_KEY}`);
+            const response = await fetch(`${url}`);
+            const responseJson = await response.json();
+            if(responseJson.error){
+                showResponseMessage(responseJson.message);
+            }else{
+                console.log(responseJson.results);
+            }
+        } catch(error){
+            showResponseMessage(error);
         }
-
-        xhr.onerror = function(){
-            showResponseMessage();
-        }
-
-        xhr.open("PUT", `${baseUrl}/edit/${book.id}`);
-
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.setRequestHeader("X-Auth-Token", "12345");
-
-        xhr.send(JSON.stringify(book));
-
     };
 
-    const removeBook = (bookId) => {
-        const xhr = new XMLHttpRequest();
+    const renderPopular = (results) => {
+        const listPopularElement = document.querySelector("#popularSlide");
+        if (listPopularElement != null){
+            listPopularElement.classList.add('carousel', 'slide', 'carousel-fade');
+            listPopularElement.setAttribute("data-ride","carousel");
+    
+            const pageNumber = document.createElement("ol");
+            pageNumber.classList.add('carousel-indicators');
+    
+            const popularItem = document.createElement("div");
+            popularItem.classList.add('carousel-inner');
+    
+            for (let i=0; i < 5;i++){
+                const item = document.createElement("div");
 
-        xhr.onload = function(){
-            const responseJson = JSON.parse(this.responseText);
-            showResponseMessage(responseJson.message);
-            getBook();
-        }
+                if (i == 0){
+                    pageNumber.innerHTML +=`
+                        <li data-target="#popularSlide" data-slide-to=${i} class="active"></li>`;
+                    item.classList.add('carousel-item', 'active');
+                }else{
+                    pageNumber.innerHTML +=`
+                        <li data-target="#popularSlide" data-slide-to=${i}></li>`;
+                    item.classList.add('carousel-item');
+                }
 
-        xhr.onerror = function(){
-            showResponseMessage();
-        }
-
-        xhr.open("DELETE", `${baseUrl}/delete/${bookId}`);
-
-        xhr.setRequestHeader("X-Auth-Token", "12345");
-
-        xhr.send();
-    };
-
-
-
-
-
-
-    /*
-        jangan ubah kode di bawah ini ya!
-    */
-
-    const renderAllBooks = (books) => {
-        const listBookElement = document.querySelector("#listBook");
-        listBookElement.innerHTML = "";
-
-        books.forEach(book => {
-            listBookElement.innerHTML += `
-                <div class="col-lg-4 col-md-6 col-sm-12" style="margin-top: 12px;">
-                    <div class="card">
-                        <div class="card-body">
-                            <a href="detail.html">
-                                <h5>(${book.id}) ${book.title}</h5>
-                            </a>
-                            <p>${book.author}</p>
-                            <button type="button" class="btn btn-danger button-delete" id="${book.id}">Hapus</button>
+                item.innerHTML += `
+                    <img src="${baseUrlImage}${results[i].backdrop_path}" class="d-block w-100" alt="cover">
+                    <div class="slide-item">
+                        <h4 class="slide-title-item">${results[i].title}</h4>
+                        <div class="slide-des-item">
+                            <p>${results[i].overview}</p>
                         </div>
-                    </div>
-                </div>
-            `;
-        });
+                        <p class="slide-det-item"><b>Release Date</b>  ${results[i].release_date}</p>
+                        <p class="slide-det-item"><b>Rate</b>  ${results[i].vote_average}</p>
+                        <p class="slide-det-item"><b>Popular</b>  ${results[i].popularity}</p>  
+                    </div>`;
 
-        const buttons = document.querySelectorAll(".button-delete");
-        buttons.forEach(button => {
-            button.addEventListener("click", event => {
-                const bookId = event.target.id;
-                removeBook(bookId);
-            })
-        })
+                popularItem.appendChild(item);
+            }
+
+            listPopularElement.appendChild(pageNumber);
+            listPopularElement.appendChild(popularItem);
+            listPopularElement.innerHTML +=`
+                <a class="carousel-control-prev" href="#popularSlide" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#popularSlide" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>`;
+        }
+
     };
 
     const showResponseMessage = (message = "Check your internet connection") => {
@@ -120,31 +110,34 @@ function main() {
 
     document.addEventListener("DOMContentLoaded", () => {
 
-        const inputBookId = document.querySelector("#inputBookId");
-        const inputBookTitle = document.querySelector("#inputBookTitle");
-        const inputBookAuthor = document.querySelector("#inputBookAuthor");
-        const buttonSave = document.querySelector("#buttonSave");
-        const buttonUpdate = document.querySelector("#buttonUpdate");
+        // const inputBookId = document.querySelector("#inputBookId");
+        // const inputBookTitle = document.querySelector("#inputBookTitle");
+        // const inputBookAuthor = document.querySelector("#inputBookAuthor");
+        // const buttonSave = document.querySelector("#buttonSave");
+        // const buttonUpdate = document.querySelector("#buttonUpdate");
 
-        buttonSave.addEventListener("click", function () {
-            const book = {
-                id: Number.parseInt(inputBookId.value),
-                title: inputBookTitle.value,
-                author: inputBookAuthor.value
-            };
-            insertBook(book)
-        });
+        // buttonSave.addEventListener("click", function () {
+        //     const book = {
+        //         id: Number.parseInt(inputBookId.value),
+        //         title: inputBookTitle.value,
+        //         author: inputBookAuthor.value
+        //     };
+        //     insertBook(book)
+        // });
 
-        buttonUpdate.addEventListener("click", function () {
-            const book = {
-                id: Number.parseInt(inputBookId.value),
-                title: inputBookTitle.value,
-                author: inputBookAuthor.value
-            };
+        // buttonUpdate.addEventListener("click", function () {
+        //     const book = {
+        //         id: Number.parseInt(inputBookId.value),
+        //         title: inputBookTitle.value,
+        //         author: inputBookAuthor.value
+        //     };
 
-            updateBook(book)
-        });
-        getBook();
+        //     updateBook(book)
+        // });
+
+        getPopular();
+        getNowplaying();
+        getUpcoming();
     });
 }
 
