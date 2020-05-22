f6d72a2a6238b27fc9209cf9b2a0aab9a94ce8cd
amary21 <taufik.amary@gmail.com>
2020-05-22 16:48:11 +0700

    web

---
+++ webpack.common.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Movie Cat",
+            hash : true,
+            alwaysWriteToDisk: true,
+            template: "./src/index.html"
+        })
+    ]
+};
