5a97e78574ee87f29faffb302ed6465e51745163
amary21 <taufik.amary@gmail.com>
2020-05-24 01:38:23 +0700

    finishing

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import "regenerator-runtime";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "./styles/main.css";
-import "./scripts/component/app-bar.js";
-import "./scripts/data/baseurl.js";
 import main from "./scripts/view/main.js";
 
 main();
